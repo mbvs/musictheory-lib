7c04b088a8e399f30a2a69d01c13740f20a4ed4e
Marcus Bauer von Schildhaue <m.bvs@zimmeracht.de>
2023-02-22 15:23:33 +0100

    remove unused code

--- src/Chord.ts
+++ src/Chord.ts
@@ -94,31 +94,5 @@ export default class Chord {
     const regexString = `^${pitch}(?:(${augumented})|(${diminished})|(${major})|(${minor}))?${rest}$`;
 
     return new RegExp(regexString);
-    // const note = '[A-G][b#]?';
-    // const altered = `(?:5|dim(5|7)?|aug5?|\\+5?|-5?)`;
-    // const minor = '(?:mi?n?)';
-    // const major = '(?:maj?|Ma?j?)';
-    // const majorableExt = `(?:6|7|9|11|13)`;
-    // const ext = `(?:4|6|7|9|11|13|6\\/9)`;
-    // const _mod = '(?:[b-](5|6|9|13)|[#+](4|5|9|11))';
-    // const mod = `(?:\\(${_mod}\\)|${_mod})`
-    // const sus = '(?:sus(2|4|24|2sus4)?)';
-    // const add = '(?:add[b#]?(?:2|4|6|7|9|11|13))';
-    // const bass = `(?:\\/${note})`;
-
-    // const lookahead = '(?=$| )';
-    // const source = `${note}${
-    //   `(?:${altered}|${
-    //     `(?:${minor}?(?:${ext}|${major}?${majorableExt})?)`
-    //     + `${mod}*${sus}?${mod}*${add}?`
-    //   })`
-    //   }${bass}?${lookahead}`;
-
-    // console.log(JSON.stringify(source));
-    // return source;
   }
-
-  // result:
-
-  //const regex = chord: new RegExp(chordRegex());
 }
