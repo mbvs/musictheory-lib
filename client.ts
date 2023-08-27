290f622ce8947ae37ac3bcda8a33a4b257f58309
mbvs <m.bvs@zimmeracht.de>
2023-08-27 12:17:41 +0200

    clean up

--- client.ts
+++
@@ -1,9 +0,0 @@
-import Note from './src/Note';
-import Interval from './src/Interval';
-import ScoreRenderer from './src/ScoreRenderer';
-import KeyRenderer from './src/KeyRenderer';
-import ObjectsToCsv from 'objects-to-csv';
-import Chord from './src/Chord';
-
-const interval = new Interval('C', 'M b2');
-Interval.identifyInterval(interval.notes[0], interval.notes[1]);
--- scratch.js
+++
@@ -1,1 +0,0 @@
-console.log('\u{266D}')
